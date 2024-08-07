87594e22eb91aba0078a3f26bdc71118561bce9a
DeveloperAnderson <acardozoar@unadvirtual.edu.co>
2024-08-06 22:29:11 -0500

    rutas ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/component/login/login.component';
-import { AgendaComponent } from './core/component/content/agenda/agenda.component';
-import { RedComponent } from './core/component/content/red/red.component';
 import { RecomendacionesComponent } from './core/component/content/recomendaciones/recomendaciones.component';
 import { UserManagementComponent } from './core/component/content/user-management/user-management.component';
-import { VacunacionComponent } from './core/component/content/vacunacion/vacunacion.component';
 import { ProductComponent } from './core/component/content/products/product.component';
 //import { UsuarioComponent } from './core/component/content/usuario/usuario.component';
 import { SidebarComponent } from './core/component/content/sidebar/sidebar.component';
@@ -26,10 +23,7 @@ export const routes: Routes = [
   { path: 'side-Bar', component: SidebarComponent },
   //{ path: 'usuario', component: UsuarioComponent },
   //{ path: 'product', component: ProductComponent },
-  { path: 'vacunacion', component: VacunacionComponent },
   { path: 'recomendaciones', component: RecomendacionesComponent },
-  { path: 'red', component: RedComponent },
-  { path: 'agenda', component: AgendaComponent },
   { path: '**', redirectTo: '/home' }
 ];
 
